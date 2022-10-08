60be6da5709b3547d13c770e84a79d6a948ff9ce
MehVahdJukaar <matteogozzi1998@gmail.com>
2022-10-08 22:38:53 +0200

    mods.toml

--- fabric/src/main/java/nl/enjarai/doabarrelroll/config/fabric/ModConfigImpl.java
+++ fabric/src/main/java/nl/enjarai/doabarrelroll/config/fabric/ModConfigImpl.java
@@ -2,6 +2,7 @@ package nl.enjarai.doabarrelroll.config.fabric;
 
 public class ModConfigImpl {
     public static boolean getModEnabled() {
+        return true;
         //cloth config stuff here. for yacl i only ever used the menu part so idk
     }
 }
