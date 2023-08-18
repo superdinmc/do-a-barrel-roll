7cd48bdc6cba0458daf78ce5a35fd8b28c7588f9
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2023-08-18 17:34:37 -0400

    Somehow I broke everything

--- src/client/java/nl/enjarai/doabarrelroll/math/SyntaxHighlighter.java
+++ src/client/java/nl/enjarai/doabarrelroll/math/SyntaxHighlighter.java
@@ -5,30 +5,56 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
 public class SyntaxHighlighter {
-	public static MutableText highlightText(String str) {
+	public static Text highlightText(String text) {
 		MutableText formattedText = Text.literal("");
-		SyntaxHighlightContext context = new SyntaxHighlightContext(str);
+		SyntaxHighlightContext context = new SyntaxHighlightContext(text);
 		
 		while (context.getCurrent() != (char)0) {
-			if (context.getCurrent() == '$') {
-				formattedText.append(formatChar(context.getCurrent(), SyntaxType.Variable));
+			if (context.getCurrent() == '$') { //variables
+				formattedText.append(String.valueOf(context.getCurrent()));
 				context.position++;
 				
 				while (isLetter(context.getCurrent()) || context.getCurrent() == '_') {
-					formattedText.append(formatChar(context.getCurrent(), SyntaxType.Variable));
+					formattedText.append(formatText(context.getCurrent(), SyntaxType.Variable));
 					context.position++;
 				}
-			} else if (Character.isDigit(context.getCurrent()) || context.getCurrent() == '.') {
-				formattedText.append(formatChar(context.getCurrent(), SyntaxType.Number));
+//			} else if (context.getCurrent() == '-' || context.getCurrent() == '+') { //unary operators
+//				if (Character.isDigit(context.peek())) {
+//					formattedText.append(formatText(context.getCurrent(), SyntaxType.Number));
+//					context.position++;
+//				} else if (isLetter(context.peek())) {
+//					formattedText.append(formatText(context.getCurrent(), SyntaxType.Function));
+//					context.position++;
+//				}
+			} else if (Character.isDigit(context.getCurrent()) || context.getCurrent() == '.') { //numbers
+				formattedText.append(formatText(context.getCurrent(), SyntaxType.Number));
 				context.position++;
-			} else if (isOperator(context.getCurrent())) {
-				formattedText.append(formatChar(context.getCurrent(), SyntaxType.Operator));
+//			} else if (isLetter(context.getCurrent())) { //functions
+//				StringBuilder builder = new StringBuilder();
+//
+//				while (isLetter(context.getCurrent())) {
+//					builder.append(context.getCurrent());
+//					context.position++;
+//				}
+//
+//				String builtResult = builder.toString();
+//
+//				if (isKeyword(builtResult) && context.getCurrent() == '(') {
+//					formattedText.append(formatText(builtResult, SyntaxType.Function));
+//				} else {
+//					formattedText.append(formatText(builtResult, SyntaxType.Error));
+//				}
+			} else if (isOperator(context.getCurrent())) { //typical operators
+				formattedText.append(formatText(context.getCurrent(), SyntaxType.Operator));
 				context.position++;
-			} else if (Character.isWhitespace(context.getCurrent())) {
+			} else if (isScope(context.getCurrent())) { //parentheses
+				formattedText.append(formatText(context.getCurrent(), SyntaxType.Scope));
+				context.position++;
+			} else if (Character.isWhitespace(context.getCurrent())) { //whitespace
 				formattedText.append(String.valueOf(context.getCurrent()));
 				context.position++;
-			} else {
-				formattedText.append(formatChar(context.getCurrent(), SyntaxType.Error));
+			} else { //errors
+				formattedText.append(formatText(context.getCurrent(), SyntaxType.Error));
 				context.position++;
 			}
 		}
@@ -36,20 +62,42 @@ public class SyntaxHighlighter {
 		return formattedText;
 	}
 	
+	public static boolean isKeyword(String str) {
+		switch (str) {
+			case "sqrt", "sin", "cos",
+					"tan", "asin", "acos",
+					"atan", "abs", "exp",
+					"ceil", "floor", "log",
+					"round", "randint", "min",
+					"max" -> {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 	public static boolean isLetter(char c) {
 		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 	}
 	
 	public static boolean isOperator(char c) {
-		return c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')';
+		return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
 	}
 	
-	public static MutableText formatChar(char ch, SyntaxType type) {
+	public static boolean isScope(char c) {
+		 return c == ',' || c == '(' || c == ')';
+	}
+	
+	public static MutableText formatText(char ch, SyntaxType type) {
 		String str = String.valueOf(ch);
-		
+		return formatText(str, type);
+	}
+	
+	public static MutableText formatText(String str, SyntaxType type) {
 		switch (type) {
 			case Variable -> {
-				return Text.literal(str).formatted(Formatting.ITALIC);
+				return Text.literal(str).formatted(Formatting.GREEN);
 			}
 			
 			case Operator -> {
@@ -62,6 +110,14 @@ public class SyntaxHighlighter {
 			
 			case Number -> {
 				return Text.literal(str).formatted(Formatting.AQUA);
+			}
+			
+			case Function -> {
+				return Text.literal(str).formatted(Formatting.BLUE);
+			}
+			
+			case Scope -> {
+				return Text.literal(str);
 			}
 		}
 		
@@ -77,9 +133,22 @@ class SyntaxHighlightContext {
 		this.rawText = raw;
 	}
 	
+	public String peek(int amount) {
+		if (position + amount >= rawText.length()) return null;
+		return rawText.substring(position, position + amount);
+	}
+	
+	public char peek() {
+		return getByIndex(position + 1);
+	}
+	
 	public char getCurrent() {
-		if (position >= rawText.length()) return (char)0;
-		return rawText.charAt(position);
+		return getByIndex(position);
+	}
+	
+	public char getByIndex(int i) {
+		if (i >= rawText.length()) return (char)0;
+		return rawText.charAt(1);
 	}
 }
 
@@ -87,5 +156,7 @@ enum SyntaxType {
 	Variable,
 	Operator,
 	Error,
+	Scope,
+	Function,
 	Number
 }
