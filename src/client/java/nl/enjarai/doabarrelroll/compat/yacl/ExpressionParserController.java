ec5d2a5b05e3f488edca5392caa17e083f9f9c82
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2023-08-18 13:33:22 -0400

    Syntax highlighting in config edit

--- src/client/java/nl/enjarai/doabarrelroll/compat/yacl/ExpressionParserController.java
+++ src/client/java/nl/enjarai/doabarrelroll/compat/yacl/ExpressionParserController.java
@@ -6,6 +6,9 @@ import dev.isxander.yacl3.gui.AbstractWidget;
 import dev.isxander.yacl3.gui.YACLScreen;
 import dev.isxander.yacl3.gui.controllers.string.IStringController;
 import dev.isxander.yacl3.gui.controllers.string.StringControllerElement;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import nl.enjarai.doabarrelroll.math.ExpressionParser;
 
 public record ExpressionParserController(Option<ExpressionParser> option) implements IStringController<ExpressionParser> {
@@ -18,7 +21,39 @@ public record ExpressionParserController(Option<ExpressionParser> option) implem
     public void setFromString(String value) {
         option.requestSet(new ExpressionParser(value));
     }
-
+    
+    @Override
+    public Text formatValue() {
+        var context = new SyntaxHighlightingContext(getString());
+        MutableText formattedText = Text.literal("");
+        
+        while (context.getCurrent() != (char)0) {
+            if (context.getCurrent() == '$') {
+                formattedText.append(formatChar(context.getCurrent(), SyntaxType.Variable));
+                context.position++;
+                
+                while (isLetter(context.getCurrent()) || context.getCurrent() == '_') {
+                    formattedText.append(formatChar(context.getCurrent(), SyntaxType.Variable));
+                    context.position++;
+                }
+            } else if (Character.isDigit(context.getCurrent()) || context.getCurrent() == '.') {
+                formattedText.append(formatChar(context.getCurrent(), SyntaxType.Number));
+                context.position++;
+            } else if (isOperator(context.getCurrent())) {
+                formattedText.append(formatChar(context.getCurrent(), SyntaxType.Operator));
+                context.position++;
+            } else if (Character.isWhitespace(context.getCurrent())) {
+                formattedText.append(String.valueOf(context.getCurrent()));
+                context.position++;
+            } else {
+                formattedText.append(formatChar(context.getCurrent(), SyntaxType.Error));
+                context.position++;
+            }
+        }
+        
+        return formattedText;
+    }
+    
     @Override
     public AbstractWidget provideWidget(YACLScreen screen, Dimension<Integer> widgetDimension) {
         return new StringControllerElement(this, screen, widgetDimension, true) {
@@ -28,4 +63,56 @@ public record ExpressionParserController(Option<ExpressionParser> option) implem
             }
         };
     }
+    
+    private boolean isLetter(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+    
+    private boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')';
+    }
+    
+    private MutableText formatChar(char ch, SyntaxType type) {
+        String str = String.valueOf(ch);
+        
+        switch (type) {
+            case Variable -> {
+                return Text.literal(str).formatted(Formatting.GREEN);
+            }
+            
+            case Operator -> {
+                return Text.literal(str).formatted(Formatting.LIGHT_PURPLE);
+            }
+            
+            case Error -> {
+                return Text.literal(str).formatted(Formatting.RED);
+            }
+            
+            case Number -> {
+                return Text.literal(str).formatted(Formatting.AQUA);
+            }
+        }
+        
+        return null;
+    }
+}
+
+class SyntaxHighlightingContext {
+    public int position = 0;
+    public String rawText;
+    
+    public SyntaxHighlightingContext(String raw) {
+        this.rawText = raw;
+    }
+    
+    char getCurrent() {
+        if (position >= rawText.length()) return (char)0;
+        return rawText.charAt(position);
+    }
+}
+
+enum SyntaxType {
+    Variable,
+    Operator,
+    Error, Number
 }
