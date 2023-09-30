c35c6997a7f13d3ce1bde2c94929f62cd70145cf
enjarai <enjarai123@gmail.com>
2023-09-30 10:11:38 +0200

    Perfection

--- src/client/java/nl/enjarai/doabarrelroll/fabric/DoABarrelRollFabricClient.java
+++ src/client/java/nl/enjarai/doabarrelroll/fabric/DoABarrelRollFabricClient.java
@@ -1,21 +1,16 @@
 package nl.enjarai.doabarrelroll.fabric;
 
 import com.bawnorton.mixinsquared.api.MixinCanceller;
+import com.bawnorton.mixinsquared.tools.MixinAnnotationReader;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import nl.enjarai.cicada.api.conversation.ConversationManager;
-import nl.enjarai.cicada.api.util.CicadaEntrypoint;
-import nl.enjarai.cicada.api.util.JsonSource;
-import nl.enjarai.cicada.api.util.ProperLogger;
 import nl.enjarai.doabarrelroll.DoABarrelRoll;
 import nl.enjarai.doabarrelroll.DoABarrelRollClient;
 import nl.enjarai.doabarrelroll.ModKeybindings;
 import nl.enjarai.doabarrelroll.config.ModConfig;
 import nl.enjarai.doabarrelroll.fabric.net.HandshakeClientFabric;
-import nl.enjarai.doabarrelroll.util.MixinCancelChecker;
 import nl.enjarai.doabarrelroll.util.StarFoxUtil;
-import org.slf4j.Logger;
 
 import java.util.List;
 
@@ -42,9 +37,9 @@ public class DoABarrelRollFabricClient implements ClientModInitializer, MixinCan
 
     @Override
     public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
-        if (mixinClassName.equals("com.anthonyhilyard.equipmentcompare.mixin.KeyMappingMixin") && !MixinCancelChecker.hasChangedPriority(mixinClassName, 0)) {
+        if (mixinClassName.equals("com.anthonyhilyard.equipmentcompare.mixin.KeyMappingMixin") && MixinAnnotationReader.getPriority(mixinClassName) == 1000) {
             DoABarrelRoll.LOGGER.warn("Equipment Compare detected, disabling their overly invasive keybinding mixin. Report any relevant issues to them.");
-            DoABarrelRoll.LOGGER.warn("If the author of Equipment Compare is reading this: see #31 on your github. Once you've fixed the issue, you can set the priority of this mixin explicitly to stop it being disabled.");
+            DoABarrelRoll.LOGGER.warn("If the author of Equipment Compare is reading this: see #31 on your github. Once the issue is fixed, you can set the priority of this mixin to anything other than 1000 to stop it being disabled.");
             return true;
         }
         return false;
