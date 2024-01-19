fd1c963109d16ca6b6a08e87bf160f6842de6720
INotDen <>
2024-01-19 20:27:57 +0600

    MidnightControls support

---
+++ src/client/java/nl/enjarai/doabarrelroll/compat/midnightcontrols/MidnightControlsPlugin.java
@@ -0,0 +1,13 @@
+package nl.enjarai.doabarrelroll.compat.midnightcontrols;
+
+import nl.enjarai.cicada.api.compat.CompatMixinPlugin;
+
+import java.util.Set;
+
+public class MidnightControlsPlugin implements CompatMixinPlugin {
+    @Override
+    public Set<String> getRequiredMods() {
+        return Set.of("midnightcontrols");
+    }
+
+}
