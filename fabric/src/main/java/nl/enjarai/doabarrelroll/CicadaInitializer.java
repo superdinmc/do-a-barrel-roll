0c21ae8e551dbb5b7e9ea2fcecba75c7463476ce
enjarai <enjarai123@gmail.com>
2024-02-22 19:26:26 +0100

    This is nice, mod lives here now

--- fabric/src/main/java/nl/enjarai/doabarrelroll/CicadaInitializer.java
+++ fabric/src/main/java/nl/enjarai/doabarrelroll/CicadaInitializer.java
@@ -8,7 +8,7 @@ public class CicadaInitializer implements CicadaEntrypoint {
     @Override
     public void registerConversations(ConversationManager conversationManager) {
         conversationManager.registerSource(
-                JsonSource.fromUrl("https://raw.githubusercontent.com/enjarai/do-a-barrel-roll/master/src/main/resources/cicada/do_a_barrel_roll/conversations.json")
+                JsonSource.fromUrl("https://raw.githubusercontent.com/enjarai/do-a-barrel-roll/meta/cicada/conversations.json")
                         .or(JsonSource.fromResource("cicada/do_a_barrel_roll/conversations.json")),
                 DoABarrelRoll.LOGGER::info
         );
